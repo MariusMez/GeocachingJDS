f4d8a2f8bf68bfd19f8cfee73ab5c261c59fd1be
Marius <marius.mez@gmail.com>
2016-10-27 11:41:41 +0200

    Bump version, fix body-parser deprecated usage in Express 4

--- cloud/app.js
+++ cloud/app.js
@@ -1,13 +1,16 @@
 
 				// These two lines are required to initialize Express in Cloud Code.
 				express = require('express');
+				var bodyParser = require('body-parser');
 				app = express();
 
 				// Global app configuration section
 				app.set('views', 'cloud/views');  // Specify the folder to find templates
 				app.set('view engine', 'ejs');    // Set the template engine
-				app.use(express.bodyParser());    // Middleware for reading request body
-
+				app.use(bodyParser.json());
+				app.use(bodyParser.urlencoded({
+					extended: true
+				}));
 				// This is an example of hooking up a request handler with a specific request
 				// path and HTTP verb using the Express routing API.
 				app.get('/', function(req, res) {
